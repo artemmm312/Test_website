a815d823e90510f31bd15acedeab16cb657db711
artemmm312 <artemmm312@gmail.com>
2021-03-29 23:52:59 +0300

    fix

--- check.php
+++ check.php
@@ -10,8 +10,29 @@ $pdo = new PDO($dsn, $user, $password, $opt);
 $birth_date = trim($_POST['year_of_birth']);
 $sql = "SELECT * FROM users WHERE bdate LIKE '$birth_date%'";
 $stmt = $pdo->query($sql);
-$count = $stmt->rowCount();
+$row = $stmt->fetchAll();
+$count = count($row);
+//print_r($row);
+if ($_POST['year_of_birth'] == "") {
+	echo "Введите год рождения!!!<a href='/Test_website/'>На главную</a>";
+} else {
+	if ($count > 0) {
+		echo "<table border='1' cellspacing='0' width='50%'><tr><th>Id</th><th>first_name</th><th>last_name</th><th>bdate</th></tr>";
+		foreach ($row as $key => $value) {
+			echo "<tr>";
+			foreach ($value as $data)
+				echo "<td>" . $data . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+		echo "<a href='/Test_website/'>На главную</a>";
+	} else echo "Юзеров с таким годом рождения нет. <a href='/Test_website/'>На главную</a>";
+}
 
+
+
+
+/*$count = $stmt->rowCount();
 if ($_POST['year_of_birth'] == "") {
 	echo "Введите год рождения!!!<a href='/Test_website/'>На главную</a>";
 } else {
@@ -25,4 +46,4 @@ if ($_POST['year_of_birth'] == "") {
 		echo "</table>";
 		echo "<a href='/Test_website/'>На главную</a>";
 	} else echo "Юзеров с таким годом рождения нет. <a href='/Test_website/'>На главную</a>";
-}
+}*/
